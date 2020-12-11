91f3644945c9b140a2d13e51abf0f4b74416e08b
Avishek Chakrabarty <avishiek@gmail.com>
2020-12-11 14:07:52 -0500

    Start a new testcase

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,8 +1,18 @@
 module.exports = {
-    'My First Thaste case'(browser){
+    'Register for NOP commerce'(browser){
+        const registerButton =  'a[class="ico-register"]';
+        const selectGenderMale = 'input[value="M]';
+        const firstNameField = 'input[id="FirstName"]';
+        const firstName = "Avi";
         browser
-            .url('https://news.ycombinator.com/')
-            .waitForElementVisible('.hnname')
-            .assert.containsText(".hnname", "Hacker News");
+            .url('https://demo.nopcommerce.com/')
+            .waitForElementVisible('.ico-register')
+            .assert.containsText(registerButton, "Register")
+            .click(registerButton)
+            .click(selectGenderMale)
+            .assert.containsText(".page-title","Register")
+            .setValue(firstNameField,firstName)
+            
+
     }
 }
